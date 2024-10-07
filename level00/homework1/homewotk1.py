7060b8c862d02149ab2531a20a405735e06e1056
nika-dobo <nikadobo20@gmail.com>
2024-10-07 23:21:04 +0400

    changes

---
+++ level00/homework1/homewotk1.py
@@ -0,0 +1,19 @@
+name = "nika"
+
+surname = "doborjginidze"
+
+age = 16
+
+height = 170
+
+knows_programming = True
+is_ugly = False
+
+isUgly = False
+
+
+print(name + " " + surname)
+
+print(name + " " + str(age))
+
+print(name + " " + surname + " " + str(age) + " " + str(height)) 
