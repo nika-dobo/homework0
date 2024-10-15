17cca79fe23b77bd20555adcc671ca5065b63136
nika-dobo <nikadobo20@gmail.com>
2024-10-15 20:57:57 +0400

    hw

---
+++ level04/homework/hw.py
@@ -0,0 +1,42 @@
+num1 = 20   #int
+num2 = 10   #int
+num3 = 45   #int
+
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+
+
+name = "nika"   #str
+surename = "doborjginidze"  #str
+
+print(name + surename)
+print("nika" + "doborjginidze")
+
+
+
+height = 168.5 #float
+
+print(height)
+
+
+x = 15  #int
+
+
+print(x + 5)
+
+
+price = 100 * 5
+
+print(price)
+
+price2 = 100
+
+print(price2 * str(5))
+
+
+
+
+
