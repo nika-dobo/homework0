4e8e9cab221bd09ef3d114cdf4f3954e3074baa8
nika-dobo <nikadobo20@gmail.com>
2024-10-15 20:31:57 +0400

    cw

---
+++ level04/classwork/cw.py
@@ -0,0 +1,24 @@
+num1 = 20
+num2 = 10
+
+
+name = "nika"   #str
+ricxvi = 5  #int
+ricxvi2 = 5.5   #float
+
+
+
+
+print(name, ricxvi, ricxvi2)
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+
+
+
+
