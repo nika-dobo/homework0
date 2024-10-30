1a3c94f16e25dd9c47df2635f20cb85897751f6c
nika-dobo <nikadobo20@gmail.com>
2024-10-30 20:09:06 +0400

    homework

---
+++ level8/homework/hw2.py
@@ -0,0 +1,11 @@
+# 2)
+# value = "10"
+# result = value * 5
+# print("Result:", result)
+
+# გაიხსენეთ მონაცემთა ტიპები და შეასწორეთ ეს კოდი 
+
+
+value = 10 #შევცვალეთ სტრნგი ინტეჯერად 
+result = value * 5
+print("Result:", result)
