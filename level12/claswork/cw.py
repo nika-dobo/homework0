17a802bc68362c86d1a7635182b671d894fc0631
nika-dobo <nikadobo20@gmail.com>
2024-11-14 14:56:49 +0400

    g

--- level12/claswork/cw.py
+++ level12/claswork/cw.py
@@ -1,2 +1,20 @@
 print("23424 == 232455 =>" + str(23424 == 232455))
 print("23424 == 23424 =>" + str(23424 == 23424))
+
+
+
+
+
+
+
+
+print(20 == 20)
+print(20 < 50)
+print(60 > 20)
+print(20 <= 20)
+print(550 >= 70)
+
+
+#bool-shi aris true da folse esegi gvanaxebs wesmaritia tu mqdaris moqmedeba 
+#operatorebi kideb rogoricaa < > ad ase shemdeg naxebs moqmdedebas meta tu naxleba tu ertamnets udris da tu arasworia mashin maoagdebs folses da tu sworia mashin trues
+
