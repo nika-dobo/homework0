3b4c15955727f6615a859989eef2c1a4ead74b9f
nika-dobo <nikadobo20@gmail.com>
2024-11-17 18:21:24 +0400

    aa

---
+++ level13/homework/hw.py
@@ -0,0 +1,93 @@
+print("1 მომხმარებელს შემოატაინეთ ორი რიცხვი და მათზე მოახდინეთ მათემატიკური ოპერაციები.")
+
+
+num1 = int(input("enter number:" ))
+num2 = int(input("enter number:" ))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(num1 == num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 != num2)
+
+
+print("2)მოიყვანეთ 10 მაგალითი შედარების ოპერატორებზე ==,!=,>,<,=<,=>")
+
+print(12 > 32)
+
+print(16 < 332)
+
+print(412 == 342)
+
+print(212 >= 32)
+
+print(72 <= 32)
+
+print(82 != 32)
+
+print(16 * 32 == 34)
+
+print(22 + 32 <= 45) 
+
+print(82 / 32 >= 23)
+
+print(72 - 32 != 48)
+
+print("3) მომხმარებელს შემოატაინეთ თავისი სახელი, გვარი, ასაკი, და შემდეგ შეაერთეთ ისე რომ გამოვიდეს წინადადება.")
+
+
+name = input("enter you name: ")
+surename = input("enter you surename: ")
+age = input("enter your age: ")
+
+
+print("chemi saxelia" + " " + name + " " + "chemi gavria" + " " + surename + " " + "chemi wlovanebaa" + " " + age)
+
+
+
+
+print("4) ახსენით რა განსხვავება ამათ შორის !=,==,ახსენით კომენტარებით ")
+
+
+#!= es aris rodesac 2 ricxvi ar unda udrises ertmanets == es kidev aucileblad unda udrid eretrmanets
+
+
+print("5) მომხმარებელს შემოატაინეთ სახელი, გვარი,ასაკი, საცხოვრებელი ადგილი .")
+
+name2 = input("enter you name: ")
+surename2 = input("enter you surename: ")
+age2 = input("enter your age: ")
+live = input("were are you leave: ")
+
+
+
+print("6) კომენტარებით ახსენით რასაც გააკეთებთ ყველაფერი.")
+
+# > am dros ricxvi unda iyos meti meore cicxvze
+# < am dros ricxvi unda iyos naklebi meore cicxvze
+# == am dros ricxvi unda udrides meore cicxvze
+# != am dros ricxvi ar unda udrides meore cicxvze
+# >= am dros ricxvi unda iyos meti an unda udrides meore cicxvze
+# <= am dros ricxvi unda iyos naklebi an unda udrides meore cicxvze
+
+
+
+print("7) მომხმარებელს შემოატაინეთ 2 რიცხვი, და შემდეგ გამოიყენეთ შედარების ოპერატორები რა რიცხვსაც შეიყვანს მომხმარებელი, მასე გამოიყენთ შედარების ოპერატორები ==,!=,<,>,<=,=>.")
+
+num3 = int(input("enter number:" ))
+num4 = int(input("enter number:" ))
+
+
+print(num3 == num4)
+print(num3 <= num4)
+print(num3 >= num4)
+print(num3 > num4)
+print(num3 < num4)
+print(num3 != num4)
+
