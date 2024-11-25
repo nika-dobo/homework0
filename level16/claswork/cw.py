646aa012ede1bf6d93ece6fa4835181afd674fd4
nika-dobo <nikadobo20@gmail.com>
2024-11-25 23:01:12 +0400

    asd

---
+++ level16/claswork/cw.py
@@ -0,0 +1,14 @@
+
+
+
+name = input("enter your name: ")
+
+
+if name == "gabrieli":
+    print(name + " " + "es saxeli aris jgufis mentori")
+
+else:
+    print(name + " " + "es saxeli aris moswavlis saxeli")    
+
+
+
