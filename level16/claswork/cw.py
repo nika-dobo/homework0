496c9bf654304925a1db3484def1910e55e25919
nika-dobo <nikadobo20@gmail.com>
2024-11-26 21:46:25 +0400

    Update cw.py

--- level16/claswork/cw.py
+++ level16/claswork/cw.py
@@ -4,8 +4,10 @@
 name = input("enter your name: ")
 
 
+
 if name == "gabrieli":
     print(name + " " + "es saxeli aris jgufis mentori")
+
 
 else:
     print(name + " " + "es saxeli aris moswavlis saxeli")    
