9f29b2f614c5b51f95b49ffc15007d2a5e44f11a
nika-dobo <nikadobo20@gmail.com>
2024-12-03 11:06:37 +0400

    asd

---
+++ level18/homework/hw1.py
@@ -0,0 +1,17 @@
+#  1)   მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი შეამოწმეთ ეს რიცხვი არის თუ არა სამის ჯერადი
+
+
+num = int(input("enter number: "))
+
+
+
+if num % 3 == 0:
+    print("aris 3 jeradi")
+else:
+     print("ar aris 3 jeradi")
+
+
+
+
+
+
---
+++ level18/homework/hw2.py
@@ -0,0 +1,13 @@
+#  2) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი შეამოწმეთ ეს რიცხვი არის თუ არა ხუთის ჯერადი
+
+
+num = int(input("enter number: "))
+
+
+
+if num % 5 == 0:
+    print("aris 3 jeradi")
+else:
+     print("ar aris 3 jeradi")
+
+     
