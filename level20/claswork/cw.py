b1796b3418ea3d0658119eeb9b5fa499981dd136
nika-dobo <nikadobo20@gmail.com>
2024-12-09 22:01:02 +0400

    sa

---
+++ level20/claswork/cw.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+for i in range(0, 100, 2):
+    print(i)
+
+    print(" ")
+
+for i in range(1, 100, 3):
+    print(i)
+
+print(" ")
+
+for i in range(100, 0):
+    print(i)
+
+print(" ")
+
+for i in range(20, 60, 4):
+    print(i)
