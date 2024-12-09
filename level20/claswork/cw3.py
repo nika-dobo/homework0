808a2c6f3e097e1173844c302f46bc5dfe0955df
nika-dobo <nikadobo20@gmail.com>
2024-12-09 22:59:58 +0400

    ას

---
+++ level20/claswork/cw3.py
@@ -0,0 +1,39 @@
+
+i = 30
+
+while i < 70:   
+    print(i)
+    i += 2
+
+u = 0
+
+while u < 30:
+    print(u)
+    u += 3
+
+y = 5
+
+while y < 50:
+    if y % 5 == 0:
+        print(y)
+    y += 1
+
+t = 5
+
+while t < 50:        
+   print(t)
+   t += 5 
+
+r = 10
+
+while r < 50:
+    if r % 2 == 0:
+        print(str(r) + "-" + "luwi")
+    else:
+        print(str(r) + "-" + "kenti")   
+    r += 1     
+
+
+
+
+    
