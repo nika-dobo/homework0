e2f7dcb3f4da3191f68c5d29e37b018db1b82ac8
nika-dobo <nikadobo20@gmail.com>
2024-12-09 22:34:13 +0400

    as

---
+++ level20/claswork/cw2.py
@@ -0,0 +1,30 @@
+
+i = 0
+
+while i < 30:
+    print(i)
+    i += 1
+
+
+u = 10
+
+while u < 20:
+    print(u)
+    u += 1 
+
+
+y = 0
+
+while y < 30:
+    if y % 2 == 0:
+        print(y)
+    y += 1
+   
+
+t = 5
+
+while t < 50:
+    if t % 2 != 0:
+        print(t)
+    t += 1 
+    
