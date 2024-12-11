8a9f4dc12d03403bb87d3634293b4e5fa534f2a8
nika-dobo <nikadobo20@gmail.com>
2024-12-11 17:33:50 +0400

    asd

---
+++ level20/homework/hw17.py
@@ -0,0 +1,10 @@
+# 18)შეიყვანეთ რიცხვი და გამოიყენეთ while ციკლი, რომ შეამოწმოთ, არის თუ არა რიცხვი დადებითი.
+
+i = int(input("enter number: "))
+
+while i == i:
+    if i > 0:
+        print("dadebiti")
+    else:
+        print("uaryfti")    
+        
