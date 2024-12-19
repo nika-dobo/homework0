abbce48148f582d4f4c809cea88b3367a3a65244
nika-dobo <nikadobo20@gmail.com>
2024-12-19 14:28:41 +0400

    sad

---
+++ level22/homework/hw.py
@@ -0,0 +1,11 @@
+# 1) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი ერთიდან ამ რიცხვამდე დაბეჭდეთ ყველა რიცხვის ჯამი
+
+ran = int(input("enter your number: "))
+
+sum = 0
+
+for i in range(ran):
+    sum += i
+    
+
+print(sum)    
