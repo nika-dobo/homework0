86880b9f3a166a6bb468e23485b518691e40b6c4
nika-dobo <nikadobo20@gmail.com>
2024-12-25 00:18:33 +0400

    SAD

--- level24/claswork/cw.py
+++ level24/claswork/cw.py
@@ -1,2 +1,20 @@
 
+print("create your acc")
 
+name = input("enter acc name: ")
+pas = input("enter acc pas: ")
+
+
+print(" ")
+print("log in")
+print(" ")
+
+acc_name = input("enter acc name: ")
+acc_pas = input("enter acc pas: ")
+
+while acc_name != name or acc_pas != pas:    
+    print("incorect")
+    input("enter corect name: ")
+    input("enter corect pas: ")
+
+print("welcome")
