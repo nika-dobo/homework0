19180b04046fa2bac975686918136918379b69b0
nika-dobo <nikadobo20@gmail.com>
2024-12-29 23:45:30 +0400

    asd

--- level26/claswork/cw.py
+++ level26/claswork/cw.py
@@ -1,1 +1,12 @@
 
+best_list = ["lamborghini", "bugati", "proche", "bmw", "mersedes", "audi", "toyota", "nissan", "honda", "ford",]
+
+print(best_list[2], best_list[5])
+
+print(best_list[0:5])
+
+print(best_list[0:])
+
+print(best_list[-3:-1])
+
+print(best_list[3:7])
