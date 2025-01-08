a9cc676ab728e9529d6ab026f35b3f8b154f7894
nika-dobo <nikadobo20@gmail.com>
2025-01-08 18:23:31 +0400

    asd

---
+++ level28/homework/hw4.py
@@ -0,0 +1,9 @@
+# 4)შექმენით სია, რომელიც შეიცავს 8 სხვადასხვა ქალაქის სახელებს. წაშალეთ მესამე და მეხუთე ქალაქის სახელი და გამოიტანეთ განახლებული სია.
+
+
+qalaqi = ["Tbilisi", "Batumi", "Kutaisi", "Rustavi", "Gori", "Zugdidi", "Poti", "Telavi"]
+
+del qalaqi[2]
+del qalaqi[4]
+
+print(qalaqi)
