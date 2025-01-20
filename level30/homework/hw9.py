3baa8fefd4a9b96a1e9812cc81fc0e398ad74fda
nika-dobo <nikadobo20@gmail.com>
2025-01-20 23:04:32 +0400

    ads

--- level30/homework/hw9.py
+++ level30/homework/hw9.py
@@ -22,18 +22,13 @@ print(list)
 
 list2 = [1, 2, 3, 4, 5]
 
-print(list2)
+new_list = []
 
-list2.pop(4)
-list2.pop(3)
-list2.pop(2)
-list2.pop(1)
-list2.pop(0)
+for i in range(1, len(list2) + 1):    
+    new_list.append(list2[-i])
+    
 
-list2.append(5)
-list2.append(4)
-list2.append(3)
-list2.append(2)
-list2.append(1)
+print(new_list)
 
-print(list2)
+
+
