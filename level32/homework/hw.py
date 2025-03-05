f51335e5adb859895e878509a526ec0f5faf7c5a
nika-dobo <nikadobo20@gmail.com>
2025-03-05 22:44:57 +0400

    ა

--- level32/homework/hw.py
+++ level32/homework/hw.py
@@ -1,15 +1,15 @@
 # 2) გააკეთეთ Reverse List და გამოიყენეთ while loop. შემოაბრუნეთ ყველა რიცხვი ლისტში. ახსნა: [1, 2, 3, 4,  5] => [5, 4, 3, 2, 1]
 
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-new_list = []
+# new_list = []
 
-i = len(num) - 1
+# i = len(num) - 1
 
-while i >= 0:
-    new_list.append(num[i])    
-    i -= 1
+# while i >= 0:
+#     new_list.append(num[i])    
+#     i -= 1
 
-print(num)
+# print(num)
 
-print(new_list)
+# print(new_list)
