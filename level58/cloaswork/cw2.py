76c2e89366b9de1f10b9e990b6cba0bc6e63871d
nika-dobo <nikadobo20@gmail.com>
2025-04-22 20:32:33 +0400

    a

---
+++ level58/cloaswork/cw2.py
@@ -0,0 +1,28 @@
+
+country = "Tbilisi"
+
+print(country.lower())
+print("AXMET".lower())
+print("HELLO".lower())
+# lower აპატარევებს ყველა ასოსო
+
+
+print(country.upper())
+print("axmet".upper())
+print("hello".upper())
+# ასიდებს ყველა ასოს
+
+
+name = "nika"
+print(name.capitalize())
+print("axmet".capitalize())
+print("hello".capitalize())
+# პირველ ასოს ადიდიებს
+
+
+rame = "aGAHLhgYYTUDTdutFDFuydfyuFDYIUfdyuiFDYfdFDfdhFDLyidfYUFDLHJAFISYDFFDLJFdolyghkjgGIFGIGUIgiuGLgifgWUIFGuisfgblisfgiSGFBslgfiu"
+print(rame.find("n"))
+print("AXMET".find("M"))
+print("HELLO".find("L"))
+# პოულობს პირველ ასოს და გვანახებს მერემდენეა
+
