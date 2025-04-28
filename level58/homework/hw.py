d74f9966e32d710288d8fee357efe0a5f0ea5d94
nika-dobo <nikadobo20@gmail.com>
2025-04-28 16:28:56 +0400

    აა

---
+++ level58/homework/hw.py
@@ -0,0 +1,48 @@
+
+country = "Tbilisi"
+
+print(country.lower())
+print("AXMET".lower())
+print("HELLO".lower())
+
+
+
+print(country.upper())
+print("axmet".upper())
+print("hello".upper())
+
+
+
+name = "nika"
+print(name.capitalize())
+print("axmet".capitalize())
+print("hello".capitalize())
+
+
+
+rame = "aGAHLhgYYTUDTdutFDFuydfyuFDYIUfdyuiFDYfdFDfdhFDLyidfYUFDLHJAFISYDFFDLJFdolyghkjgGIFGIGUIgiuGLgifgWUIFGuisfgblisfgiSGFBslgfiu"
+print(rame.find("n"))
+print("AXMET".find("M"))
+print("HELLO".find("L"))
+
+
+list = [1,2,3,45,6,7,8,45,34,354,435,43,45,45,4,5,4,4343,34,534,534,5345,34]
+
+
+print(len(country))
+print(len(rame))
+print(len(list))
+
+
+list.append(4)
+list.append(345)
+list.append(23453)
+
+
+list.insert(4,6)
+list.insert(3,35)
+list.insert(-1,34)
+
+list.pop(3)
+list.pop(-3)
+list.pop(8)
