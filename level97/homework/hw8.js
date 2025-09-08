abb76a19e7f7c4cf8c1997bbe7c3ad235dda8810
nika-dobo <nikadobo20@gmail.com>
2025-09-08 22:29:30 +0400

    Update hw8.js

--- level97/homework/hw8.js
+++ level97/homework/hw8.js
@@ -1,9 +1,9 @@
 //8)მომხმარებელს შემოატანინეთ თქვენი სახელი,გვარი,ასაკი,საცხოვრებელი ადგილი,დაბადების თარიღი(წლებში),სიმაღლე ,წონა და შეინახეთ ცვლადებში ,გამოიყენეთ prompt ფუნქცია,
 
-let = prompt("enter your name: ")
-let = prompt("enter your surename: ")
-let = prompt("enter your age: ")
-let = prompt("where are you live: ")
-let = prompt("enter your date : ")
-let = prompt("enter your height: ")
-let = prompt("enter your weith: ")
+let name = prompt("enter your name: ")
+let surename = prompt("enter your surename: ")
+let age = prompt("enter your age: ")
+let live = prompt("where are you live: ")
+let date = prompt("enter your date : ")
+let height = prompt("enter your height: ")
+let weith = prompt("enter your weith: ")
