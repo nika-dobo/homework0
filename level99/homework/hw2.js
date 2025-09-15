743be27b77fb73438287ea37448bf8eaa612bc3b
nika-dobo <nikadobo20@gmail.com>
2025-09-15 23:07:29 +0400

    a

--- level99/homework/hw2.js
+++ level99/homework/hw2.js
@@ -1,7 +1,7 @@
 //2)მომხმარებელს შემოატანინეთ სახელი და რიცხვი,შემდეგ შეამოწმეთ,თუ მომხმარებლის მიერ შემოტანილი სახელი არის "გიორგი" და მომხმარებლის მიერ შემოტანილი რიცხვი არის 18 ზე მეტი მაშნ დაუკონსოლლოგეთ "your name is giorgi and you are adult" თუ მომხმარებლის მიერ შემოტანილი სახელი არის ნიკა და ასაკი არის 18 ზე ნაკლები და 11 ზე მეტი დაუკონსოლლოგეთ რომ "your name is nika and you are younger" თუ მომხმარებლის მიერ შემოტანილი სახელი არის "saba" ან "irakli" და ასაკი არის 11 ზე ნაკლები და 5 ზე მეტი მაშინ დაუკონსოლლოგეთ რომ "you have nice name but you are too young" სხვა შემთხვევაში დაუპრინტე რომ you are not born yet
 
 let name = prompt("enter name");
-let age = prompt("enter age");
+let age = Number(prompt("enter age"));
 
 if (name == "გიორგი" && age > 18) {
   console.log("your name is giorgi and you are adult");
--- level99/homework/hw5.js
+++ level99/homework/hw5.js
@@ -1,25 +1,12 @@
 //5)მომხმარებელს შემოატანინეთ რაიმე რიცხვი(მაგრამ ეს რიცხვია არ ვიცით სტრინგია თუ ნამბერი) შენი დავალებაა შეამოწმო თუ მომხმარებლის მიერ შემოტანილი რიცხვის ტიპი არის სტრინგი მაშინ დაუკონსოლლოგე რომ "you entered string number,so you are wrong" სხვა შემთხვევაში ანუ თუ მომხმარებელმა შემოიტანა რიცხხვი რომლის ტიპი არის number მაშინ დაატრიალეთ ფორ ციკლი 1 დან ამ რიცხვამდე და გამოიტანეთ ყველა კენტი რიცხვი კონსოლში(წინაზე ვისაუბრეთ ფუნქციაზე რომელიც იგებდა ელემენტის ტიპს,გაიხსენეთ თუა რადა მოიძიეთ,მე არ მომწეროთ არ დაგეხმარებით იჭყლიტეთ თავები ამ დავალებებზე,you will gain skill of looking up something by yourself <3 :D
 
+let num = Number(prompt("enter number"));
 
-let num = prompt("enter number");
 
-
-if (Number(num) === num) {
+if (isNaN(Number(num))) {
   console.log("you entered string number,so you are wrong");
+} else {
   for (let i = 1; i < num; i++) {
     console.log(i);
   }
-} else {
-  console.log("you entered string number,so you are wrong");
 }
-
-
-
-
-// if (isNaN(Number(num))) {
-//   console.log("you entered string number,so you are wrong");
-// } else {
-//   for (let i = 1; i < num; i++) {
-//     console.log(i);
-//   }
-// }
