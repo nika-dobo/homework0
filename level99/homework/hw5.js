94e0b08573975ae4f29e2dce1ce41ac6c8aa06fc
nika-dobo <nikadobo20@gmail.com>
2025-09-15 21:44:40 +0400

    a

--- level99/homework/hw5.js
+++ level99/homework/hw5.js
@@ -1,11 +1,25 @@
 //5)მომხმარებელს შემოატანინეთ რაიმე რიცხვი(მაგრამ ეს რიცხვია არ ვიცით სტრინგია თუ ნამბერი) შენი დავალებაა შეამოწმო თუ მომხმარებლის მიერ შემოტანილი რიცხვის ტიპი არის სტრინგი მაშინ დაუკონსოლლოგე რომ "you entered string number,so you are wrong" სხვა შემთხვევაში ანუ თუ მომხმარებელმა შემოიტანა რიცხხვი რომლის ტიპი არის number მაშინ დაატრიალეთ ფორ ციკლი 1 დან ამ რიცხვამდე და გამოიტანეთ ყველა კენტი რიცხვი კონსოლში(წინაზე ვისაუბრეთ ფუნქციაზე რომელიც იგებდა ელემენტის ტიპს,გაიხსენეთ თუა რადა მოიძიეთ,მე არ მომწეროთ არ დაგეხმარებით იჭყლიტეთ თავები ამ დავალებებზე,you will gain skill of looking up something by yourself <3 :D
 
+
 let num = prompt("enter number");
 
-if (isNaN(Number(num))) {
+
+if (Number(num) === num) {
   console.log("you entered string number,so you are wrong");
-} else {
   for (let i = 1; i < num; i++) {
     console.log(i);
   }
+} else {
+  console.log("you entered string number,so you are wrong");
 }
+
+
+
+
+// if (isNaN(Number(num))) {
+//   console.log("you entered string number,so you are wrong");
+// } else {
+//   for (let i = 1; i < num; i++) {
+//     console.log(i);
+//   }
+// }
