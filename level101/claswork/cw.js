75db28dd4455fc0cfa872f797714bad4f20598c8
nika-dobo <nikadobo20@gmail.com>
2025-09-19 13:03:50 +0400

    ზ

---
+++ level101/claswork/cw.js
@@ -0,0 +1,11 @@
+let i = 1;
+
+while (i < 100) {
+  console.log(i);
+  i++;
+}
+
+while (i < 1000) {
+  console.log(i, "i love you");
+  i++;
+}
---
+++ level101/claswork/cw2.js
@@ -0,0 +1,52 @@
+let num = Number(prompt("enter num"));
+
+let i = 1;
+
+while (i < num) {
+  console.log(i);
+  i++;
+}
+
+let u = 1;
+
+while (u < num) {
+  console.log(i);
+  if (i % 2 == 0) {
+    console.log(i, "luwi");
+  }
+  i++;
+}
+
+let y = 1;
+
+while (y < num) {
+  if (i % 2 != 0) {
+    console.log(i, "kenti");
+  }
+  i++;
+}
+
+let x = 1;
+
+while (x < num) {
+  if (i % 2 == 0) {
+    console.log(i, "luwi");
+  } else {
+    console.log(i, "kenti");
+  }
+  i++;
+}
+
+let o = 100;
+
+while (o > 0) {
+  console.log(o);
+  o--;
+}
+
+let p = 1;
+let number = 0;
+
+while (p < num) {
+  number++;
+}
