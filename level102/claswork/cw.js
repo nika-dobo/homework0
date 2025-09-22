b81cda55c3205a49fab5a6bbb5345f90239864cf
nika-dobo <nikadobo20@gmail.com>
2025-09-23 00:33:06 +0400

    a

---
+++ level102/claswork/cw.js
@@ -0,0 +1,16 @@
+function hello() {
+  let name = prompt("enter name");
+  console.log(`hello ${name}`);
+}
+
+function rame() {
+  let name = prompt("enter name");
+  let age = prompt("enter name");
+  let city = prompt("enter name");
+  console.log(
+    `hello , my name is ${name} and i am ${age} years old, i live in ${city}`
+  );
+}
+
+hello();
+rame();
---
+++ level102/claswork/hw2.js
@@ -0,0 +1,21 @@
+function jami(num1, num2) {
+  console.log(num1 + num2);
+}
+
+jami(5, 2);
+jami(32, 45);
+jami(932, 28);
+jami(95, 865);
+jami(59, 45);
+
+function rame(num1) {
+  for (let i = 1; i < num1; i++) {
+    console.log(i);
+  }
+}
+
+rame(54)
+rame(456)
+rame(5234)
+rame(524)
+rame(72)
