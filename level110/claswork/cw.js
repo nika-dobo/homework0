968825034267aaddefe9635e52453bf0733bd45d
nika-dobo <nikadobo20@gmail.com>
2025-10-20 23:26:10 +0400

    Create cw.js

---
+++ level110/claswork/cw.js
@@ -0,0 +1,12 @@
+let main = [1,2,3,4,5,6,7,8,9,10,11,12,14,511546,567,578,678,678,345,355,36839,589,58,53853]
+
+let odd = []
+let even = []
+
+for(let i = 0; i < main.length; i++){
+    if(main % 2 == 0){
+        even.push[main[i]]
+    }else{
+        odd.push[main[i]]
+    }
+}
