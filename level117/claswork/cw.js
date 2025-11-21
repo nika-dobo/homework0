9ba785594469a624d0916c0f280416b2214a0520
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2025-11-21 21:20:14 +0400

    Update cw.js

--- level117/claswork/cw.js
+++ level117/claswork/cw.js
@@ -1,3 +1,4 @@
-let body = document.body
+let body = document.body;
 
-body.innerHTML = "<div style='width=150px; height=300px'><img src='https://www.gokite.travel/wp-content/uploads/schema-and-structured-data-for-wp/2.-Experience-Thrills-Top-Rides-at-IMG-Adventure-Park-Dubai-1200x675.webp'><h1 class='heading'>blablabla</h1><p class='par'>balasdas</p><button class='butt'>click me</button></div>"
+body.innerHTML =
+  "<div style='width=150px; height=300px'><img src='https://www.gokite.travel/wp-content/uploads/schema-and-structured-data-for-wp/2.-Experience-Thrills-Top-Rides-at-IMG-Adventure-Park-Dubai-1200x675.webp'><h1 class='heading'>blablabla</h1><p class='par'>balasdas</p><button class='butt'>click me</button></div>";
