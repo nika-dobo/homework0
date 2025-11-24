3c8e347521a74d89d31dcc6a400c82b77a42bdb5
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2025-11-24 16:52:05 +0400

    a

--- level118/homework/hw5.js
+++ level118/homework/hw5.js
@@ -3,3 +3,16 @@
 // ----- თუ ველი ცარიელია → გამოჩნდება შეცდომის შეტყობინება input ველის გვერდით.
 // -----თუ ველი არაა ცარიელი → გამოჩნდება alert ს Mensagem-ით "Form submitted!".
 
+let form = document.getElementById("myForm");
+let inp = document.getElementById("myInput");
+
+function fun(event) {
+  event.preventDefault();
+  if (inp.value !== "") {
+    alert("Form submitted!");
+  } else {
+    alert("error");
+  }
+}
+
+form.addEventListener("submit", fun);
