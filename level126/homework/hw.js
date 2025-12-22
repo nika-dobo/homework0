32424e798a81b34ec45680da8a00b3738923b6be
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2025-12-22 12:20:10 +0400

    a

---
+++ level126/homework/hw.js
@@ -0,0 +1,36 @@
+const body = document.body;
+const video = document.getElementById("video");
+const newYear = new Date();
+
+function time() {
+  const now = new Date();
+  let distance = newYear - now;
+
+  if (distance <= 0) {
+    document.getElementById("dey").textContent = 0;
+    document.getElementById("hours").textContent = 0;
+    document.getElementById("min").textContent = 0;
+    document.getElementById("sec").textContent = 0;
+  }
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("dey").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("min").textContent = minutes;
+  document.getElementById("sec").textContent = seconds;
+}
+
+setInterval(time, 1000);
+
+const now = new Date();
+let distance = newYear - now;
+if (distance <= 0) {
+  let txt = document.createElement("h5");
+  txt.textContent = "heppy new year";
+  body.appendChild(txt);
+  video.style.display = "block";
+}
