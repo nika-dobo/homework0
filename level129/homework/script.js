d11e408d2d0f29b8c43a1583cf0d0c01b60d2aaf
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-01-13 00:25:28 +0400

    a

--- level129/homework/script.js
+++ level129/homework/script.js
@@ -4,6 +4,7 @@ document.getElementById("form").addEventListener("submit", function (e) {
   function errorInput(input) {
     input.value = "";
     input.style.borderColor = "red";
+    input.style.borderWidth = "2px";
   }
 
   function clearError(input) {
@@ -12,61 +13,84 @@ document.getElementById("form").addEventListener("submit", function (e) {
 
   function checkName() {
     const name = document.getElementById("name");
+    const display = document.getElementById("card-name");
     clearError(name);
 
-    document.getElementById("card-name").textContent = name.value;
+    display.textContent = name.value;
 
-    if (name.value.length < 3) {
+    let hasNumber = false;
+
+    for (let i = 0; i < name.value.length; i++) {
+      if (
+        name.value[i] >= "0" &&
+        name.value[i] <= "9" &&
+        name.value[i] !== " "
+      ) {
+        hasNumber = true;
+        break;
+      }
+    }
+
+    if (name.value.length < 3 || hasNumber) {
       errorInput(name);
+      display.textContent = "Jane Appleseed";
     }
   }
 
   function checkCard() {
     const card = document.getElementById("card-id");
+    const display = document.getElementById("id");
     clearError(card);
 
-    document.getElementById("id").textContent = card.value;
+    display.textContent = card.value;
 
     if (card.value.length !== 16 || isNaN(card.value)) {
       errorInput(card);
+      display.textContent = "0000 0000 0000 0000";
     }
   }
 
   function checkMonth() {
     const mm = document.getElementById("mm");
+    const display = document.getElementById("ma");
     clearError(mm);
 
-    document.getElementById("ma").textContent = mm.value;
+    display.textContent = mm.value;
 
     if (
       mm.value.length !== 2 ||
       isNaN(mm.value) ||
-      mm.value < 1 ||
-      mm.value > 12
+      parseInt(mm.value) < 1 ||
+      parseInt(mm.value) > 12
     ) {
       errorInput(mm);
+      display.textContent = "00";
     }
   }
 
   function checkYear() {
     const yy = document.getElementById("yy");
+    const display = document.getElementById("yr");
     clearError(yy);
 
-    document.getElementById("yr").textContent = yy.value;
+    display.textContent = yy.value;
 
     if (yy.value.length !== 2 || isNaN(yy.value)) {
       errorInput(yy);
+      display.textContent = "00";
     }
   }
 
   function checkCVC() {
     const cvc = document.getElementById("cvc");
+    const display = document.getElementById("cvv");
     clearError(cvc);
 
-    document.getElementById("cvv").textContent = cvc.value;
+    display.textContent = cvc.value;
 
     if (cvc.value.length !== 3 || isNaN(cvc.value)) {
       errorInput(cvc);
+      display.textContent = "000";
     }
   }
 
