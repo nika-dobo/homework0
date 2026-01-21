c263cf89f9367704d93f1b61b9f8c11acfd01089
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-01-21 17:39:54 +0400

    a

--- level131/product-list-with-cart-main/script.js
+++ level131/product-list-with-cart-main/script.js
@@ -1,32 +1,45 @@
 const addCart = document.getElementsByClassName("add-to-cart");
 const mainImg = document.getElementsByClassName("main-img");
 const countP = document.getElementsByClassName("count");
+const plus = document.getElementsByClassName("plus");
+const minus = document.getElementsByClassName("minus");
+const addBtn = document.getElementsByClassName("add");
+const addedBtn = document.getElementsByClassName("added");
 
 for (let i = 0; i < addCart.length; i++) {
   addCart[i].addEventListener("click", function () {
+    let currentCount = Number(countP[i].textContent) || 1;
+
     mainImg[i].style.border = "2px solid #a65c43";
     addCart[i].style.background = "#c83b0e";
-    document.getElementsByClassName("add")[i].style.display = "none";
-    document.getElementsByClassName("added")[i].style.display = "flex";
-    countP[i].textContent = 1;
+    addBtn[i].style.display = "none";
+    addedBtn[i].style.display = "flex";
+
     countP[i].style.color = "white";
+    countP[i].textContent = currentCount;
   });
 
-  const plus = document.getElementsByClassName("plus");
-  const minus = document.getElementsByClassName("minus");
-  
-  plus[i].addEventListener("click", function () {
-    let count = Number(countP[i].textContent);
-    count++;
-    countP[i].textContent = count;
+  plus[i].addEventListener("click", function (e) {
+    e.stopPropagation();
+    let currentCount = Number(countP[i].textContent);
+    currentCount++;
+    countP[i].textContent = currentCount;
   });
 
-  minus[i].addEventListener("click", function () {
-    let count = Number(countP[i].textContent);
+  minus[i].addEventListener("click", function (e) {
+    e.stopPropagation();
+    let currentCount = Number(countP[i].textContent);
 
-    if (count > 0) {
-      count--;
-      countP[i].textContent = count;
+    if (currentCount > 1) {
+      currentCount--;
+      countP[i].textContent = currentCount;
+    } else if (currentCount === 1) {
+      countP[i].textContent = 1;
+
+      mainImg[i].style.border = "none";
+      addCart[i].style.background = "white";
+      addBtn[i].style.display = "flex";
+      addedBtn[i].style.display = "none";
     }
   });
 }
