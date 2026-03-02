42116255e25cf11c559fc572532a59ca0e1dc4e2
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-03-02 16:41:31 +0400

    a

---
+++ level143/claswork/cw.js
@@ -0,0 +1,18 @@
+let name = prompt("enter name:");
+
+switch (name) {
+  case "girogi":
+    console.log("your name is girogi");
+    break;
+  case "lasha":
+    console.log("your name is lasha");
+    break;
+  case "nika":
+    console.log("your name is nika");
+    break;
+  case "buba":
+    console.log("your name is buba");
+    break;
+  default:
+    console.log("your name is not any of them");
+}
