9c5a77c7c14602617fb04062a22704c66d82d1fd
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-04-17 18:11:10 +0400

    a

--- level154/claswork/cw2.js
+++ level154/claswork/cw2.js
@@ -1,19 +1,3 @@
-// გაქვს მასივი:
-
-// let arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
-
-// შ
-// ექმენი Map, სადაც:
-
-// key = ელემენტი
-// value = რამდენჯერ გვხვდება
-
-// საბოლოოდ უნდა მიიღო მსგავსი:
-
-// apple → 3
-// banana → 2
-// orange → 1
-
 let arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
 
 let map = new Map();
