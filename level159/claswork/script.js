d6b2dab59eda856dc7e79c766d652a8f617db3a2
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-05-15 23:09:40 +0400

    a

--- level159/claswork/script.js
+++ level159/claswork/script.js
@@ -2,12 +2,16 @@ const countDetector = document.getElementById("count");
 const formTodo = document.getElementById("form-todo");
 const todoItems = document.getElementById("todo-items");
 const deleteAllBtn = document.getElementById("btn-delete-all");
+const sort = document.getElementById("sort");
+
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 let doneTasks = 0;
-let totalTasks = JSON.parse(localStorage.getItem("tasks")).length || 0;
+let totalTasks = JSON.parse(localStorage.getItem("tasks"))?.length || 0;
 
-function createToduItem(inpTxt) {
+function createToduItem(inpTxt, isCompleted) {
+  isCompleted = isCompleted || false;
+
   let main_div = document.createElement("div");
   main_div.id = "item";
 
@@ -16,6 +20,28 @@ function createToduItem(inpTxt) {
 
   let checkbox = document.createElement("button");
   checkbox.id = "todo_items_left";
+  checkbox.addEventListener("click", () => {
+    if (isCompleted) {
+      checkbox.style.backgroundColor = "#0d0d0d";
+      p.style.textDecoration = "none";
+      p.style.color = "#ffffff";
+      doneTasks--;
+    } else {
+      checkbox.style.backgroundColor = "#88ab33";
+      p.style.textDecoration = "line-through";
+      p.style.color = "#ffffff77";
+      doneTasks++;
+    }
+    countDetector.textContent = `${doneTasks}/${totalTasks}`;
+    isCompleted = !isCompleted;
+    tasks.find((item) => {
+      if (item.text === p.textContent) {
+        item.isCompleted = isCompleted;
+      }
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
 
   let p = document.createElement("p");
   p.id = "text-todo";
@@ -51,6 +77,18 @@ function createToduItem(inpTxt) {
   main_div.appendChild(right);
 
   todoItems.appendChild(main_div);
+
+  sort.addEventListener("change", () => {
+    if (sort.value === "complited" && isCompleted) {
+      main_div.style.display = "flex";
+    } else if (sort.value === "not-complited" && !isCompleted) {
+      main_div.style.display = "flex";
+    } else if (sort.value === "all") {
+      main_div.style.display = "flex";
+    } else {
+      main_div.style.display = "none";
+    }
+  });
 }
 
 formTodo.addEventListener("submit", (e) => {
@@ -58,7 +96,9 @@ formTodo.addEventListener("submit", (e) => {
   let textInput = e.target.todo_item_text.value.trim();
   if (textInput === "" || tasks.includes(textInput)) return;
   e.target.todo_item_text.value = "";
-  tasks.push(textInput);
+  tasks.push({
+    text: textInput,
+  });
   localStorage.setItem("tasks", JSON.stringify(tasks));
   createToduItem(textInput);
   totalTasks++;
@@ -75,6 +115,9 @@ deleteAllBtn.addEventListener("click", () => {
 });
 
 for (let i = 0; i < tasks.length; i++) {
-  createToduItem(tasks[i]);
+  createToduItem(tasks[i].text, tasks[i].isCompleted);
+  if (tasks[i].isCompleted) {
+    doneTasks++;
+  }
   countDetector.textContent = `${doneTasks}/${totalTasks}`;
 }
