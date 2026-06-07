87793fbb81c8673ec7d4502f8d40bab9329430fc
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-06-07 23:06:46 +0400

    sa

--- level168/claswork/cw.js
+++ level168/claswork/cw.js
@@ -1,8 +1,14 @@
-const h1City = document.getElementById("ctiy-name");
-const temperature = document.getElementById("temperature");
+const h1City = document.getElementById("city-name");
+const mainTemp = document.getElementById("temp");
 const wind = document.getElementById("wind");
-const form = document.getElementById("form");
-const weather = document.getElementById("weather");
+const form = document.getElementById("search-form");
+const weatherDesc = document.getElementById("weather");
+const humidity = document.getElementById("humidity");
+const cloudy = document.getElementById("cloudy");
+const tempMax = document.getElementById("temp-max");
+const tempMin = document.getElementById("temp-min");
+const timeDisplay = document.getElementById("time");
+const weatherIcon = document.getElementById("weather-icon");
 
 function weatherapi(city) {
   let http = new XMLHttpRequest();
@@ -10,10 +16,27 @@ function weatherapi(city) {
   http.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       let response = JSON.parse(this.responseText);
-      h1City.textContent = city;
-      temperature.textContent = `${response.current.temp_c}`;
-      wind.textContent = `${response.current.wind_kph}`;
-      weather.textContent = response.current.condition.text
+      h1City.textContent = response.location.name;
+      mainTemp.textContent = `${response.current.temp_c}°`;
+      wind.textContent = `${response.current.wind_kph}km/h`;
+      weatherDesc.textContent = response.current.condition.text.toUpperCase();
+      humidity.textContent = `${response.current.humidity}%`;
+      cloudy.textContent = `${response.current.cloud}%`;
+      tempMax.textContent = `${response.current.temp_c}°`;
+      tempMin.textContent = `${response.current.temp_c}°`;
+      weatherIcon.src = response.current.condition.icon;
+
+      let date = new Date(response.location.localtime);
+      timeDisplay.textContent =
+        date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }) +
+        " - " +
+        date.toLocaleDateString("en-GB", {
+          weekday: "long",
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        });
+
       console.log(response);
     }
   };
