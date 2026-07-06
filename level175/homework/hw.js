a95a517c356b1efcc2ec8affeb492f27564948af
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-07-06 21:05:06 +0400

    ა

---
+++ level175/homework/hw.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("fetchBtn");
+const resultDiv = document.getElementById("result");
+
+btn.addEventListener("click", async () => {
+  try {
+    resultDiv.innerHTML = "<em>იტვირთება... 🐾</em>";
+
+    const response = await fetch("https://catfact.ninja/fact");
+
+    const data = await response.json();
+
+    resultDiv.innerHTML = `<strong>ფაქტი:</strong> ${data.fact}`;
+  } catch (error) {
+    resultDiv.innerHTML = `<span style="color: red;">შეცდომა: მონაცემები ვერ ჩაიტვირთება 😿</span>`;
+  }
+});
