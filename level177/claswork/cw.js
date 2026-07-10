a9c5e836cc3ae7b533c6f8e1361e1a13c4cbf5fe
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-07-10 23:07:24 +0400

    a

--- level177/claswork/cw.js
+++ level177/claswork/cw.js
@@ -1,1 +1,21 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let accInfo = {
+    name: e.target.name.value.trim(),
+    email: e.target.email.value.trim(),
+    phone: e.target.phone.value.trim(),
+    password: e.target.password.value.trim(),
+  };
+
+  
+  
+});
+
+
+
+// 1.რეგისტრაცია (Sign Up, Sign In) + (1.5: localstorage)
+// 2.შესვლის თანავე უნდა იყოს 20 პროდუქტის ჩამონათვალი
+// 3.პროდუქტზე ინფორმაცია
