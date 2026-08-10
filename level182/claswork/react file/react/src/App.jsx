0e4dac210073377a44e831caa140eac96bd28219
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-08-10 22:53:34 +0400

    a

---
+++ level182/claswork/react file/react/src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello my beautiful world</h1>
+      <div className="video">
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1051045324229341%2F&show_text=true&width=380&t=0"
+          width="380px"
+          height="591px"
+          scrolling="no"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1569332054907615%2F&show_text=true&width=380&t=0"
+          width="380"
+          height="591"
+          scrolling="no"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F2001318353789910%2F&show_text=true&width=267&t=0"
+          width="267"
+          height="591"
+          scrolling="no"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+
+      <img
+        src="https://scontent.ftbs6-2.fna.fbcdn.net/v/t39.30808-6/680703374_122132779995001518_5274364397142411010_n.jpg?stp=dst-jpg_tt6&cstp=mx1997x1411&ctp=s1997x1411&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=p_trd-RGUX8Q7kNvwG4UJOt&_nc_oc=Adqmd-hbJKFurxa2f9p218ZUQG1uvuJPUuj_Nprn757sCt0vfadoFeIKY7mxGPMiS8E&_nc_zt=23&_nc_ht=scontent.ftbs6-2.fna&_nc_gid=VcFRQfYug8M4B9FtrPiGjw&_nc_ss=7b2a8&oh=00_AQHajsj_KcD9j2K5WFp50muOvY2ODHcukFHhtL9NWYXTHw&oe=6A7FDE85"
+        alt=""
+        width="520px"
+      />
+    </>
+  );
+}
+
+function Header() {
+  return <header>Georgia Adventure Seasons • GEOAS</header>;
+}
+
+export default App;
+
