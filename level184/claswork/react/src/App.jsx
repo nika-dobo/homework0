017956f45547860f7c70dcca5503775128efaf9a
nika dobo <149093833+nika-dobo@users.noreply.github.com>
2026-08-14 22:49:57 +0400

    a

--- level184/claswork/react/src/App.jsx
+++ level184/claswork/react/src/App.jsx
@@ -1,22 +1,11 @@
 import "./style.css";
-import Header from "./header";
-import Footer from "./footer";
-import Home from "./home";
-import Card from "./card";
 
 function App() {
+  let userAge = prompt("Enter your age");
+
   return (
     <>
-      <Header />
-      <Home />
-      
-      <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
-      </div>
-
-      <Footer />
+      <p>{userAge >= 18 ? "You are an big boy" : "You are a litel boy"}</p>
     </>
   );
 }
